e29280adabe00cf3a93bb0cce568847395df9630
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-09-27 21:56:35 +0100

    Add test for batched channel

---
+++ tests/BatchedChan.hs
@@ -0,0 +1,20 @@
+import Control.Concurrent
+import Control.Monad
+
+import BatchedChan as BC
+
+main :: IO ()
+main = do
+    bchan <- newBatchedChan 100
+    void $ forkIO $ produce bchan
+    forever $ do
+        a <- BC.tryReadChan bchan
+        case a of
+            Nothing -> print "Empty"
+            Just e -> print e
+
+produce :: BatchedChan Int -> IO ()
+produce chan = go 0
+  where go n = do BC.writeChan chan n
+                  yield
+                  go (n+1)
