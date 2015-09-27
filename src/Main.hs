c1a2f8744eb0635b924caa79b64c59eabed219e4
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-09-28 00:54:14 +0100

    Fix test build

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import Control.Monad
 
 import qualified Data.ByteString.Char8 as B8
--- tests/BatchedChan.hs
+++ tests/BatchedChan.hs
@@ -1,20 +1,20 @@
 import Control.Concurrent
 import Control.Monad
 
-import BatchedChan as BC
+import Control.Concurrent.Chan.Bounded.Batched as CBB
 
 main :: IO ()
 main = do
     bchan <- newBatchedChan 100
     void $ forkIO $ produce bchan
     forever $ do
-        r <- BC.tryReadChan bchan
+        r <- CBB.tryReadChan bchan
         case r of
             Nothing -> print "Empty"
             Just e -> print e
 
 produce :: BatchedChan Int -> IO ()
 produce chan = go 0
-  where go n = do BC.writeChan chan n
+  where go n = do CBB.writeChan chan n
                   yield
                   go (n+1)
