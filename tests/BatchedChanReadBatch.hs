38076ccc28c59b93d86823eb4a840e50fe8be50e
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-09-28 01:29:59 +0100

    Add batch read test

---
+++ tests/BatchedChanReadBatch.hs
@@ -0,0 +1,18 @@
+import Control.Concurrent
+import Control.Monad
+
+import Control.Concurrent.Chan.Bounded.Batched as CBB
+
+main :: IO ()
+main = do
+    bchan <- newBatchedChan 10
+    void $ forkIO $ produce bchan
+    forever $ do
+        batch <- CBB.readBatchChan (\_ cnt -> (cnt == 10, cnt+1)) (1 :: Int) bchan
+        print batch
+
+produce :: BatchedChan Int -> IO ()
+produce chan = go 0
+  where go n = do CBB.writeChan chan n
+                  yield
+                  go (n+1)
