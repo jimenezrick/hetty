1c31e7911f56101363b501bff53f1d445c5a770e
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-09-29 18:36:54 +0100

    Fix benchmarks

--- benchmarks/Chan.hs
+++ benchmarks/Chan.hs
@@ -6,10 +6,13 @@ import Control.Concurrent
 import qualified Control.Concurrent.Chan.Bounded.Batched as CBB
 
 read :: Chan Int -> IO ()
-read = void . readChan
+read c = do writeChan c 0
+            void $ readChan c
 
 read1 :: CBB.Chan Int -> IO ()
-read1 = void . CBB.readChan
+read1 c = do CBB.writeChan c 0
+             void $ CBB.readChan c
 
 readBatch1 :: CBB.Chan Int -> IO ()
-readBatch1 c = void $ CBB.readBatchChan (\_ cnt -> (cnt == 1, undefined)) (1 :: Int) c
+readBatch1 c = do CBB.writeChan c 0
+                  void $ CBB.readBatchChan (\_ cnt -> (cnt == 1, undefined)) (1 :: Int) c
--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,5 +1,4 @@
 import Control.Concurrent
-import Control.Monad
 import Criterion.Main
 
 import qualified Control.Concurrent.Chan.Bounded.Batched as CBB
@@ -9,12 +8,7 @@ import Chan
 main :: IO ()
 main = do
     chan <- newChan
-    void $ forever $ writeChan chan 0
-
-    bchan <- CBB.newChan 10000
-    void $ forever $ CBB.writeChan bchan 0
-
-    threadDelay 500000
+    bchan <- CBB.newChan 100
 
     defaultMain [
         bench "Chan.read" $ whnfIO (Chan.read chan)
