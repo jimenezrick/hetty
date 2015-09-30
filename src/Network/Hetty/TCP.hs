cf022d1c6023eeb43a8c42a0cf6267ea97026e3e
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-10-01 00:55:11 +0100

    Tidy up TCP types

--- src/Network/Hetty/TCP.hs
+++ src/Network/Hetty/TCP.hs
@@ -1,24 +1,31 @@
-module TCP where
+module Network.Hetty.TCP where
 
 import Control.Monad
+import Data.Word
 
-import qualified Data.ByteString as B
 import qualified System.Socket as S
 import qualified System.Socket.Family.Inet as SI
 
 import Network.Hetty.Transport
 
-createTransport :: S.Socket S.Inet S.Stream S.TCP -> Transport a
-createTransport sock = {
-    send t bs = void $ S.send (unTCP t) (B.concat bs) mempty
-  , close = S.close . unTCP
+type Socket = S.Socket S.Inet S.Stream S.TCP
+
+type Port = Word16
+
+createTCPTransport :: Socket -> Transport
+createTCPTransport sock = Transport {
+    send  = \buf -> void $ S.send sock buf mempty
+  , close = S.close sock
   }
 
---instance Locator TCP where
-foo = {
-    connect (LocalService port) = do
-        sock <- S.socket
-        S.connect sock addr
-        return $ TCP sock
-      where addr = SI.SocketAddressInet SI.loopback $ SI.Port port
+localLocator :: Locator Port
+localLocator = Locator {
+    connect = \(Service port) -> liftM createTCPTransport $ connectLoopbackPort port
   }
+
+connectLoopbackPort :: Port -> IO Socket
+connectLoopbackPort port = do
+    s <- S.socket
+    S.connect s addr
+    return s
+  where addr = SI.SocketAddressInet SI.loopback $ SI.Port port
