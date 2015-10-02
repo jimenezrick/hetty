adf6f1786fbddf1a118fc034dd42c130afc1d8b2
Ricardo Catalinas Jiménez <r@untroubled.be>
2015-10-02 23:58:08 +0100

    Tudy app Pub module

--- src/Network/Hetty/Protocol/Pub.hs
+++ src/Network/Hetty/Protocol/Pub.hs
@@ -1,52 +1,53 @@
 module Network.Hetty.Protocol.Pub where
 
-import Data.Word
 import Data.Foldable
 import Data.IORef
-import Data.ByteString.Lazy
-import Control.Monad
-import System.Socket
-import System.Socket.Family.Inet
-
+import Data.ByteString hiding (map)
 import Control.Concurrent hiding (Chan)
+import Control.Monad
+
 import Control.Concurrent.Chan.Bounded.Batched as CBB
+import Network.Hetty.Transport
 
 -- XXX: Move to its own module
-data Conn a = Conn {
-    chan :: Chan a
-  , sock :: Socket Inet Stream TCP
+data Conn = Conn {
+    chan      :: Chan
+  , transport :: Transport
   }
 
-newConn :: Socket Inet Stream TCP -> IO Conn a
-newConn sock = do chan <- CBB.newChan
-                  return Conn { chan = chan, sock = sock}
+chanSize :: Int
+chanSize = 1024
 
+newConn :: Transport -> IO Conn
+newConn tport = do c <- CBB.newChan chanSize
+                   return Conn { chan = c, transport = tport }
 
-newtype ContextRef a = ContextRef {
-    unContextRef :: IORef Context a
-  } deriving Functor
+newtype ContextRef = ContextRef {
+    unContextRef :: IORef Context
+  }
 
-data Context a = Context { conns :: [Conn a] }
+data Context = Context { conns :: [Conn] }
 
 -- TODO: monadic interface?
-withContext :: ContextRef a -> (Context -> IO ()) -> IO ()
+withContext :: ContextRef -> (Context -> IO ()) -> IO ()
 withContext ref f = do ctx <- readIORef (unContextRef ref)
                        f ctx
 
 
 
-newContext :: IO (ContextRef a)
+newContext :: IO ContextRef
 newContext = newIORef (Context []) >>= return . ContextRef
 
-updateContext :: (Context -> Context) -> ContextRef a -> IO ()
+updateContext :: (Context -> Context) -> ContextRef -> IO ()
 updateContext update ref = atomicModifyIORef' (unContextRef ref) (\ctx -> (update ctx, ()))
 
-addConn :: Conn -> Context -> Context
-addConn conn ctx@Context{conns = conns} = ctx { conns = conn:conns }
+addConn :: ContextRef -> Conn -> IO ()
+addConn ref conn = do
+    updateContext (\ctx@Context{conns = cs } -> ctx { conns = conn:cs }) ref
 
 
 publish :: ByteString -> Context -> IO ()
-publish msg ctx = traverse_ (push msg) (conns ctx)
+publish msg ctx = traverse_ (push msg) $ map chan $ conns ctx
   where push msg conn = do done <- CBB.tryWriteChan conn msg
                            if done
                                then return ()
@@ -54,18 +55,6 @@ publish msg ctx = traverse_ (push msg) (conns ctx)
 
 
 
-connectPort :: ContextRef a -> Word16 -> IO ()
-connectPort ref port = do
-    sock <- socket :: IO (Socket Inet Stream TCP)
-    let addr = SocketAddressInet loopback $ Port port
-    connect sock addr
-
-    conn <- newConn sock
-    updateContext (addConn conn) ref
-
-    runConn conn
-
-    void $ send sock "hello\n" mempty
 
 
 
@@ -74,4 +63,4 @@ activateConn :: Conn -> IO ()
 activateConn conn = void $ forkIO $ do
     forever $ do
         batch <- CBB.readBatchChan $ chan conn
-        send (sock conn) batch mempty
+        send (transport conn) batch
